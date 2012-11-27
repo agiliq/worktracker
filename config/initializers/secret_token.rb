0275776815de70b983b8ae79d713cdf9b34f5f75
Bhaskar <bhaskar@agiliq.com>
2012-11-27 22:16:43 +0530

    Initial Commit - generated new app and added rails-backbone to gemfile. Adding all files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Worksummarizer::Application.config.secret_token = 'fd598feb00b051f2225fc2a3628207a12c05400b8fad619e016a07f652355a0d80e256ff4f6cb105b8f16a18a4bf46d43c10db042f453fdc152bdcda90d86bf2'
